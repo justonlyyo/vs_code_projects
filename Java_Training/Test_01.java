147be6fd77325f00ee05f5b7ea9636137968e52c
walle <583736361@qq.com>
2018-12-25 18:37:03 +0800

    1

--- Java_Training/Test_01.java
+++ Java_Training/Test_01.java
@@ -1,7 +1,29 @@
-class Test_01{
+
+
+public class Test_01{
     public static void main(String[] args){
         System.out.println("Hello world!");
+        Person p = new Person("张三");
+        p.sayHello();
+        for(int i = 0; i < 10; i++){          
+            switch(i % 2){
+                case 0:
+                System.out.println(String.format("%d偶数", i));
+                break;
+                default:
+                System.out.println(String.format("%d奇数", i));
+            }
+        }
     }
 }
 
-class Person{}
+class Person{
+    String name;
+    public Person(String name){
+        this.name = name;
+    }
+    public void sayHello(){
+        String str = "my name is " + this.name;
+        System.out.println(str);
+    }
+}
